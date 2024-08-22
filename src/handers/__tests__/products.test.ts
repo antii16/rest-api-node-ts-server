8c29410e8b9f77875f4c61a580f45a39936977bc
Antonella <98277771+antii16@users.noreply.github.com>
2024-08-22 21:47:25 +0200

    Probando producto por ID

--- src/handers/__tests__/products.test.ts
+++ src/handers/__tests__/products.test.ts
@@ -57,7 +57,6 @@ describe('GET /api/products', () => {
     it('should check if api/products url exists', async ()=> {
         const response = await request(server).get('/api/products')
         expect(response.status).not.toBe(404)
-    
     })
     it('GET a JSON response with products', async ()=> {
         const response = await request(server).get('/api/products')
@@ -68,5 +67,29 @@ describe('GET /api/products', () => {
 
         expect(response.status).not.toBe(404)
         expect(response.body).not.toHaveProperty('errors')
+    })
+})
+
+describe('GET /api/products/:id', ()=>{
+    it('should return a 404 response for a non-existe product', async ()=>{
+        const productId = 2000
+        const response = await request(server).get(`/api/products/${productId}`)
+        expect(response.status).toBe(404)
+        expect(response.body).toHaveProperty('error')
+        expect(response.body.error).toBe('Producto No Encontrado')
+    })
+
+    it('should check a valid ID in the URL', async ()=> {
+        const response = await request(server).get('/api/products/not-valid-url')
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+        expect(response.body.errors[0].msg).toBe('ID no válido')
+    })
+
+    it('get a JSON response for a single product', async ()=> {
+        const response = await request(server).get('/api/products/1')
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('data')
     })
 })
