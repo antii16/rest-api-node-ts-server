18312168e13cb2b5d971769bea2a3fc47fae22ec
Antonella <98277771+antii16@users.noreply.github.com>
2024-08-23 19:48:32 +0200

    Documentando productos y atributos de productos

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,35 @@ import { Router } from "express";
 import { body, param } from 'express-validator'
 import { createProduct, deleteProduct, getProductById, getProducts, updateAvailability, updatedProduct } from "./handlers/products";
 import { handleInputErrors } from "./middleware";
+
 const router = Router()
+/**
+ * @swagger
+ * components: 
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              properties: 
+ *                  id:
+ *                      type: integer
+ *                      description:  The Product ID
+ *                      example: 1
+ *                  name:
+ *                      type: string
+ *                      description:  The Product name
+ *                      example: Monitor Curvo de 49 Pulgadas
+ *                  price:
+ *                      type: number
+ *                      description:  The Product price
+ *                      example: 300
+ *                  availability:
+ *                      type: boolean
+ *                      description:  The Product availavility
+ *                      example: true
+ * 
+ * 
+ */
+
 
 // Routing
 router.get('/', getProducts)
